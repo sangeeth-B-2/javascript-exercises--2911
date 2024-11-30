001d41de7d0b4d9d3e3f61cb07e6610ca889d633
sangeeth-B-2 <akashbala503@gmail.com>
2024-11-30 12:14:43 +0530

    CLONED REPO FORM THEODINPROJECT

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function (text, no) {
+    
+    if (no < 0) {
+    
+    return "ERROR";
+        
+    }
+    let repeat = ""; 
+    let i = 0;
+    while ( i < no)  {
+            repeat += text;
+        i++;
+        }
+        return repeat;
+    
 };
 
 // Do not edit below this line
