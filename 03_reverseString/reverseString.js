103044f33b56596ae84329e8c0e6ea3f59d116fb
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-01 18:24:51 +0530

    used `for` loop to reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseTheString = '';
+    if (string.length<0){
+        return '';
+    }else if (string.length>0) {
+        for (let i=(string.length); i--;) {
+        reverseTheString = reverseTheString.concat(string.charAt(i));
+        }
+    
+    }
+    return reverseTheString;
+    
 };
 
 // Do not edit below this line
