6cd37b528f043b01c3b55b621d0fa36156989b82
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-01 20:34:53 +0530

    used for loop for sum and isInteger() for checking both limits are postive whole numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sumAll= 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    if (start > end) {
+        [start, end] = [end, start]; // Swap the values of start and end
+    }
+    if (start >= 0 && end >=0 ){  
+    for (let i =start;i<= end;i++){
+         sumAll +=i;        
+        
+        }
+        return sumAll;
+    } else return "ERROR";
 };
 
 // Do not edit below this line
