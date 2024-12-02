eebb0e26df6427b38952a2232126718280509f06
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-02 13:39:40 +0530

    used for loop for sum of array & multiply of array. used Math.pow for one number to the power of another number

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (a, b) {
+  return (a + b);
 
 };
 
-const power = function() {
-	
+const subtract = function (a, b) {
+  return (a - b);
 };
 
-const factorial = function() {
-	
+const sum = function (arr) {
+  let n = arr.length
+  let result = 0;
+  for(let i=0;i<n;i++){
+  result +=arr[i];
+  }
+  return result;
+};
+
+const multiply = function (arr) {
+  let n = arr.length
+  let result = 1;
+  for(let i=0;i<n;i++){
+  result *=arr[i];
+  }
+  return result;
+  
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b)
+};
+
+const factorial = function (n) {
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result;
+  } else {
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+    }
+  }
+  return result;
+
 };
 
 // Do not edit below this line
