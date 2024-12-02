1cd0ef270f6d29fca8363e7daf00a89fddaad5ef
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-02 11:59:08 +0530

    used mathematical formula and Math.round() function to round down to one decimal as requested.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let Celsius= Math.round(((Fahrenheit - 32 ) * 5/9)*10 )/10; 
+  return Celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  
+  let Fahrenheit= Math.round(((Celsius * 9/5)+32)*10) /10;
+  return Fahrenheit;
 };
 
 // Do not edit below this line
