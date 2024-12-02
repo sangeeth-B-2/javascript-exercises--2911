31c4ef7770f59bb34b8a0982d247b24113ac5f3b
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-02 14:46:05 +0530

    used .replace(/[\s\p{P}]/gu, '') method to remove all space and punctuation. and split,reverse,join methods to reverse.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+const reverseString = function (str){
+    
+    let pureString = str.replace(/[\s\p{P}]/gu, '');
+    return pureString.split('').reverse().join('');
+}    
+let reverseStringFinal = reverseString(string);
+let OgStringCleaned = string.replace(/[\s\p{P}]/gu, '').toUpperCase();
+return OgStringCleaned === reverseStringFinal.toUpperCase();
 };
 
 // Do not edit below this line
