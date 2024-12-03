6f4d814133df786122a247fc67f2c04c09dc7443
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-03 09:32:19 +0530

    if its string covert to number. use 2 var and reassign after loop for next iteration of fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (typeof n ==="string"){
+        n= Number(n);
+    }
+    if (n < 0) return 'OOPS';
+    if (n === 0 )return 0; 
+    if (typeof n === "number") {
+        let a = 0, b = 1;
+        for (let i = 2; i <= n; i++) {
+            let fib = a + b;
+            a = b;
+            b = fib;
+        }
+        return b;
+    }
+    
 };
 
 // Do not edit below this line
