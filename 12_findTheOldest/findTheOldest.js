79f6604f5944ec3e8ee86859b2fb9b6ec0b7bcb0
sangeeth-B-2 <akashbala503@gmail.com>
2024-12-03 15:33:41 +0530

    used reduce() function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        let currentYear = new Date().getFullYear();
+        let deathYear = person.yearOfDeath || currentYear;
 
+        let age = deathYear - person.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        return age > oldestAge ? person : oldest;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
